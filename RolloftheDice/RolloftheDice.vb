7f8faa5df1c8458b884238714407ebf188732970
Justin Bell <Juxin203@gmail.com>
2024-09-29 19:45:26 -0600

    init

---
+++ RolloftheDice/RolloftheDice.vb
@@ -0,0 +1,16 @@
+﻿'Justin Bell
+'RCET0235
+'Fall 2024
+'Roll of the Dice
+' link
+
+Option Compare Text
+Option Explicit On
+Option Strict On
+Module RolloftheDice
+
+    Sub Main()
+
+    End Sub
+
+End Module
